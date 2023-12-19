92ee078454b97d78e7c6d2f3b98953548b2b1db2
ruizleah <leruiz1@s.sfusd.edu>
2023-12-19 11:46:18 -0800

    player scene works but janky

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var velocity
-@export var speed = 400.0
+
+@export var speed = 900.0
 @onready var area_2d = $Area2D
 @onready var collision_shape_2d = $Area2D/CollisionShape2D
 
@@ -12,19 +12,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocity = Vector2.ZERO
+	var velocity = Vector2.ZERO
 	
 	if Input.is_action_just_pressed("left"):
-		velocity.y -= 1
+		velocity.x -= 1
 	if Input.is_action_just_pressed("down"):
-		velocity.y -= 1
+		velocity.y += 1
 	if Input.is_action_just_pressed("right"):
-		velocity.y += 1
+		velocity.x += 1
 	if Input.is_action_just_pressed("up"):
-		velocity.y += 1
+		velocity.y -= 1
 	
-	velocity 
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
 	
+	position += velocity * delta
 
 
 
