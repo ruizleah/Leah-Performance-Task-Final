cc9fc8025c48f705e86c5b0cfe4735b474753b85
ruizleah <leruiz1@s.sfusd.edu>
2023-12-19 11:23:31 -0800

    bg music looped

--- player.gd
+++ player.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-@export var speed
+@export var speed = 400.0
+@onready var area_2d = $Area2D
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +11,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	 
+	pass
 	
 
+
+
+func _on_area_2d_body_entered(body):
+	pass # Replace with function body.
+	
